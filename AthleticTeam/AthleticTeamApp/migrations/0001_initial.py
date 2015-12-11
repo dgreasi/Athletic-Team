c3ffae768330eafb137f84e00933b2fea170b208
Dgreasidis <temeteron94@gmail.com>
2015-12-11 20:43:54 +0200

    Fixed base. Need a fix from players. Commited from Stephanos. Bug: rendering a list.

--- AthleticTeam/AthleticTeamApp/migrations/0001_initial.py
+++ AthleticTeam/AthleticTeamApp/migrations/0001_initial.py
@@ -2,11 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
@@ -17,6 +19,7 @@ class Migration(migrations.Migration):
                 ('first_name', models.CharField(max_length=30, blank=True)),
                 ('last_name', models.CharField(max_length=30, blank=True)),
                 ('info', models.TextField(blank=True)),
+                ('image', models.ImageField(upload_to=b'photos/', blank=True)),
                 ('position', models.CharField(max_length=30, blank=True)),
             ],
             options={
@@ -65,6 +68,7 @@ class Migration(migrations.Migration):
                 ('first_name', models.CharField(max_length=30, blank=True)),
                 ('last_name', models.CharField(max_length=30, blank=True)),
                 ('info', models.TextField(blank=True)),
+                ('image', models.ImageField(upload_to=b'photos/', blank=True)),
                 ('height', models.DecimalField(null=True, max_digits=3, decimal_places=2, blank=True)),
                 ('weight', models.PositiveSmallIntegerField(null=True, blank=True)),
                 ('birth_date', models.DateField(null=True, blank=True)),
@@ -76,6 +80,23 @@ class Migration(migrations.Migration):
             options={
                 'abstract': False,
             },
+        ),
+        migrations.CreateModel(
+            name='Ranking',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('power_arm', models.PositiveSmallIntegerField(null=True, blank=True)),
+                ('power_body', models.PositiveSmallIntegerField(null=True, blank=True)),
+                ('power_legs', models.PositiveSmallIntegerField(null=True, blank=True)),
+                ('speed', models.PositiveSmallIntegerField(null=True, blank=True)),
+                ('team_play', models.PositiveSmallIntegerField(null=True, blank=True)),
+                ('co_op', models.PositiveSmallIntegerField(null=True, blank=True)),
+                ('rate_of_pos', models.PositiveSmallIntegerField(null=True, blank=True)),
+                ('two_shots', models.PositiveSmallIntegerField(null=True, blank=True)),
+                ('three_shots', models.PositiveSmallIntegerField(null=True, blank=True)),
+                ('owner', models.ForeignKey(to=settings.AUTH_USER_MODEL, null=True)),
+                ('player', models.ForeignKey(to='AthleticTeamApp.Player')),
+            ],
         ),
         migrations.CreateModel(
             name='Team',
@@ -113,5 +134,9 @@ class Migration(migrations.Migration):
             model_name='coachingstaffmember',
             name='team',
             field=models.ForeignKey(to='AthleticTeamApp.Team'),
+        ),
+        migrations.AlterUniqueTogether(
+            name='ranking',
+            unique_together=set([('player', 'owner')]),
         ),
     ]
